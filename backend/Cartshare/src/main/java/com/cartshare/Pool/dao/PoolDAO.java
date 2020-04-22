00d7f0dbd1b5220918e3980eeddb90a367e1fc1e
jayasurya17 <jayasurya1796@gmail.com>
2020-04-22 15:19:46 -0700

    Removed warnings

--- backend/Cartshare/src/main/java/com/cartshare/Pool/dao/PoolDAO.java
+++ backend/Cartshare/src/main/java/com/cartshare/Pool/dao/PoolDAO.java
@@ -1,19 +1,12 @@
 package com.cartshare.Pool.dao;
 
-import javax.persistence.EntityManager;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.cartshare.models.Pool;
-import com.cartshare.models.User;
-import com.cartshare.repositories.PoolRepository;
-import com.cartshare.repositories.UserRepository;
+import com.cartshare.models.*;
+import com.cartshare.repositories.*;
 
 @Repository
 public class PoolDAO {
-	@Autowired
-    private EntityManager entityManager;
 	
 	@Autowired
     UserRepository userRepository;
